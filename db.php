17f376d9cd35585785b6869524b51c6c0b4e033a
Mike Obi <mikeobi9@gmail.com>
2023-08-18 19:46:01 +0100

    added comment to connection string db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@
     $password = "";
     $database = "sample_web_project";
     
-    $con = mysqli_connect($server,$username,$password,$database);
+    $con = mysqli_connect($server,$username,$password,$database); // Database connection string
     //$con = mysqli_connect("localhost","root","","login_register");
     // Check connection
     if (mysqli_connect_errno()){
